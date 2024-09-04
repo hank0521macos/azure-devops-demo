7e46e2c872e157015334fe185f940bacb18b4269
Meng Yu Wu <hank0521macos@hotmail.com>
2024-09-04 08:27:44 +0000

    Updated SnakeGameController.java

--- src/main/java/com/demo/controller/SnakeGameController.java
+++ src/main/java/com/demo/controller/SnakeGameController.java
@@ -24,7 +24,7 @@ public class SnakeGameController {
 
     @GetMapping("/")
     public String index() {
-        // TODO: Test Pipeline
+        // TODO: Test Pipeline 1
         return "index";
     }
 
